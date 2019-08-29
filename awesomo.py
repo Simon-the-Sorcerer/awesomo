2ec0a9c1d75c13d7dab03857242d8e99c682e3e3
Simon <schaefer@fidion.de>
2019-08-29 14:55:14 +0200

    Logging hinzugefügt

--- awesomo.py
+++ awesomo.py
@@ -1,5 +1,14 @@
 import discord
 import config
+import logging
+
+# Logging Einstellungen
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 
 client = discord.Client()
 
