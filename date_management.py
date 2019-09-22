450ceb7211223545d0bb0cd953ab91f43d6c7a61
Simon <simon81186@aol.com>
2019-09-22 11:37:14 +0200

    Termine aus Datenbank anzeigen

--- date_management.py
+++ date_management.py
@@ -1,11 +1,30 @@
 '''
 Modul zum Verwalten des Zockkalenders ACHTUNG: WIP!
 '''
+
+import sqlite3
+import discord
+
 async def show(ctx):
     '''
     Termine ausgeben
     '''
-    await ctx.send('dates: show')
+    connection = sqlite3.connect('calendar.db')
+    cursor = connection.cursor()
+    sql = 'SELECT Date, Description FROM dates'
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    
+    embed = discord.Embed(title='Anstehende Zocktermine', description='', color=0xab4642)
+    dates = ''
+    descriptions = ''
+    for row in rows:
+        dates += row[0] + '\n'
+        descriptions += row[1] + '\n'
+    embed.add_field(name='Termin', value=dates, inline=True)
+    embed.add_field(name='Beschreibung', value=descriptions, inline=True)
+
+    await ctx.send(embed=embed)
     await ctx.message.delete()
 
 async def add(ctx, date, description):
